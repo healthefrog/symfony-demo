d1525a9f35e69ccec1f0156a80de48bd0fb39c54
Ben Spencer <ben.spencer@healthforge.io>
2016-11-30 17:44:45 +0000

    Disable http cache.

--- web/app.php
+++ web/app.php
@@ -29,16 +29,6 @@ include_once __DIR__.'/../var/bootstrap.php.cache';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
-// When using the HTTP Cache to improve application performance, the application
-// kernel is wrapped by the AppCache class to activate the built-in reverse proxy.
-// See http://symfony.com/doc/current/book/http_cache.html#symfony-reverse-proxy
-$kernel = new AppCache($kernel);
-
-// If you use HTTP Cache and your application relies on the _method request parameter
-// to get the intended HTTP method, uncomment this line.
-// See http://symfony.com/doc/current/reference/configuration/framework.html#http-method-override
-Request::enableHttpMethodParameterOverride();
-
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
